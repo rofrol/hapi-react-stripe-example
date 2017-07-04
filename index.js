bf3d5ec74ab13e06fe98c6ec9c410ece459e0b04
Roman Frołow <rofrol@gmail.com>
2017-07-04 15:12:24 +0200

    react works

---
+++ index.js
@@ -0,0 +1,83 @@
+// inspired by http://jamesknelson.com/learn-raw-react-ridiculously-simple-forms/
+
+var cities = [
+    'Warsaw',
+    'Washington',
+    'Piaseczno',
+    'Chicago'
+];
+
+['div', 'ul', 'li', 'input', 'form'].map(function (elem) { this[elem] = React.DOM[elem]; });
+
+/*
+ * Components
+ */
+
+
+function CitiesForm(props) {
+    return (
+        div({ id: 'dropdiv' },
+            input({
+                type: 'text',
+                placeholder: 'City',
+                value: props.value.name,
+                onChange: props.onChange,
+            }),
+            ul({ id: 'drop' },
+                cities.reduce(function (acc, element) {
+                    var input = props.value.name.toLowerCase();
+                    if (props.value.name.trim() !== '' && element.toLowerCase().substring(0, input.length) === input) {
+                        acc.push(li(null, element));
+                    }
+                    return acc;
+                }, [])
+            )
+        )
+    );
+}
+
+CitiesForm.propTypes = {
+    value: React.PropTypes.object.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+};
+
+/*
+ * Constants
+ */
+
+var SEARCH_TEMPLATE = { name: "" };
+
+
+/*
+ * Actions
+ */
+
+function updateSearch(e) {
+    setState({ newSearch: { name: e.target.value } });
+}
+
+/*
+ * Model
+ */
+
+
+// The app's complete current state
+var state = {};
+
+// Make the given changes to the state and perform any required housekeeping
+function setState(changes) {
+    Object.assign(state, changes);
+
+    ReactDOM.render(
+        CitiesForm(Object.assign({}, state, {
+            value: state.newSearch,
+            onChange: updateSearch,
+        })),
+        document.getElementById('app')
+    );
+}
+
+// Set initial data
+setState({
+    newSearch: SEARCH_TEMPLATE,
+});
--- server.js
+++ server.js
@@ -1,28 +1,53 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Path = require('path');
 
 const server = new Hapi.Server();
-server.connection({port: 3003, host: 'localhost'});
-
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(request, reply) {
-        reply('Hello, world!');
-    }
-});
+server.connection({ port: 3003, host: 'localhost' });
 
 server.route({
     method: 'GET',
     path: '/{name}',
-    handler: function(request, reply) {
+    handler: function (request, reply) {
         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
     }
 });
 
+server.register(require('inert'), (err) => {
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('index.html');
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/index.js',
+        handler: function (request, reply) {
+            reply.file('index.js');
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/node_modules/{file*}',
+        handler: {
+            directory: {
+                path: 'node_modules'
+            }
+        }
+    })
+
+});
+
+
+
 server.start(err => {
-    if(err) {
+    if (err) {
         throw err;
     }
     console.log(`Server running at ${server.info.uri}`);
